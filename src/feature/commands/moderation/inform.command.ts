dbef3df39735c24a7a6870f0a33a2845af9dbeec
Haiyn <corexaren@gmail.com>
2024-11-18 22:39:40 +0100

    add commands that are still todo

---
+++ src/feature/commands/moderation/inform.command.ts
@@ -0,0 +1,1 @@
+// TODO: Create inform command
---
+++ src/feature/commands/staffmail/staff-mail-block.command.ts
@@ -0,0 +1,7 @@
+// TODO: Implement a blocking feature that blocks a user from using staffmail
+/**
+ * block via a new command that has to be run in an anonymous staffmail
+ * blocked users cant send anything anonymous
+ * When a user is blocked, dm them instructions to appeal non-anonymously
+ * Add a warning when sending anonymous messages about abuse
+ */
