25bdf6fa528936910fca632a57e98e9b3a29fa20
haiyn <corexaren@gmail.com>
2025-12-26 22:38:57 +0100

    mongo db docker compose fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa_stafffm; git fetch'
 ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa_stafffm; git pull -f'
 
-docker compose build
-docker compose up -d
+docker compose pull
+docker compose up -d --build
